cdd2bea0ec5cfda96d490176fb6285a8ffd31de8
ellievoid <ellievoiid@gmail.com>
2026-08-16 16:03:12 +0200

    feature: you can now paint on the canvas

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const canva = document.querySelector('.canva');
 const slider = document.getElementById('grid-slider');
 
+let isMouseDown = false;
+
+window.addEventListener('mousedown', () => {
+    isMouseDown = true;
+});
+
+window.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
+
 function createGrid(size) {
     /* reset old grid */
     canva.innerHTML = '';
 
     canva.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     canva.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
     /* create performant fragment */
     const fragment = document.createDocumentFragment();
     const totalPixels = size * size;
@@ -15,6 +26,15 @@ function createGrid(size) {
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
         fragment.appendChild(pixel);
+
+        pixel.addEventListener('mousedown', () => {
+            pixel.style.backgroundColor = "var(--color-cyan-300)";
+        })
+        pixel.addEventListener('mouseenter', () => {
+           if (isMouseDown === true) {
+             pixel.style.backgroundColor = "var(--color-cyan-300)";
+            }
+        })
     }
     canva.appendChild(fragment);
 }
